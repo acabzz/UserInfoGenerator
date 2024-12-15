a7d981f6761be696176ed097660808f7d3b62a05
acabzzz <acabidig@gmail.com>
2024-12-16 00:29:18 +0800

    added a meesage for default endpoint

--- src.js
+++ src.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
-import { faker } from '@faker-js/faker';
 
 const unitedStates = {"AL": "Alabama","AK": "Alaska","AZ": "Arizona","AR": "Arkansas","CA": "California","CO": "Colorado","CT": "Connecticut","DE": "Delaware","FL": "Florida","GA": "Georgia","HI": "Hawaii","ID": "Idaho","IL": "Illinois","IN": "Indiana","IA": "Iowa","KS": "Kansas","KY": "Kentucky","LA": "Louisiana","ME": "Maine","MD": "Maryland","MA": "Massachusetts","MI": "Michigan","MN": "Minnesota","MS": "Mississippi","MO": "Missouri","MT": "Montana","NE": "Nebraska","NV": "Nevada","NH": "New Hampshire","NJ": "New Jersey","NM": "New Mexico","NY": "New York","NC": "North Carolina","ND": "North Dakota","OH": "Ohio","OK": "Oklahoma","OR": "Oregon","PA": "Pennsylvania","RI": "Rhode Island","SC": "South Carolina","SD": "South Dakota","TN": "Tennessee","TX": "Texas","UT": "Utah","VT": "Vermont","VA": "Virginia","WA": "Washington","WV": "West Virginia","WI": "Wisconsin","WY": "Wyoming","AS": "American Samoa","DC": "District of Columbia","FM": "Federated States of Micronesia","GU": "Guam","MH": "Marshall Islands","MP": "Northern Mariana Islands","PW": "Palau","PR": "Puerto Rico","VI": "Virgin Islands","AA": "Armed Forces Americas","AE": "Armed Forces Europe","AP": "Armed Forces Pacific"};
 
@@ -41,6 +40,10 @@ function s(l) {
     selectedFunction === "n" ? n(length) : s(length);
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.json({Author: 'Made with ❤️ by PUSA'});
+})
 
 app.get('/:country', (req, res) => {
         let country = req.params.country || 'us';
