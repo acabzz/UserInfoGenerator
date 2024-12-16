0c71acdf8ace20104e6ee977a1c6102e1044678d
acabzzz <acabidig@gmail.com>
2024-12-16 12:04:34 +0800

    better output message ( more organized )

--- src.js
+++ src.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import UserAgent from 'user-agents';
 
 const unitedStates = {"AL": "Alabama","AK": "Alaska","AZ": "Arizona","AR": "Arkansas","CA": "California","CO": "Colorado","CT": "Connecticut","DE": "Delaware","FL": "Florida","GA": "Georgia","HI": "Hawaii","ID": "Idaho","IL": "Illinois","IN": "Indiana","IA": "Iowa","KS": "Kansas","KY": "Kentucky","LA": "Louisiana","ME": "Maine","MD": "Maryland","MA": "Massachusetts","MI": "Michigan","MN": "Minnesota","MS": "Mississippi","MO": "Missouri","MT": "Montana","NE": "Nebraska","NV": "Nevada","NH": "New Hampshire","NJ": "New Jersey","NM": "New Mexico","NY": "New York","NC": "North Carolina","ND": "North Dakota","OH": "Ohio","OK": "Oklahoma","OR": "Oregon","PA": "Pennsylvania","RI": "Rhode Island","SC": "South Carolina","SD": "South Dakota","TN": "Tennessee","TX": "Texas","UT": "Utah","VT": "Vermont","VA": "Virginia","WA": "Washington","WV": "West Virginia","WI": "Wisconsin","WY": "Wyoming","AS": "American Samoa","DC": "District of Columbia","FM": "Federated States of Micronesia","GU": "Guam","MH": "Marshall Islands","MP": "Northern Mariana Islands","PW": "Palau","PR": "Puerto Rico","VI": "Virgin Islands","AA": "Armed Forces Americas","AE": "Armed Forces Europe","AP": "Armed Forces Pacific"};
 
@@ -161,18 +162,41 @@ app.get('/:country', (req, res) => {
           const emailUsername = shuffle(emailUsernames)[0];
           const cleanedName = emailUsername.replace(/[^A-Za-z0-9\s]/g, "");
           const EmailAddress = `${cleanedName}@${emailDomain}`;
+          const chromeAgent = new UserAgent({
+            userAgent: /Chrome/,
+            deviceCategory: 'desktop',
+            platform: /(Win|Mac)/, 
+        });
+          const firefoxAgent = new UserAgent({
+            userAgent: /Firefox/,
+            deviceCategory: 'desktop',
+            platform: /(Win|Mac)/, 
+        });
         
         res.json(
             { 
-                FirstName: nameParts[0], 
-                LastName: nameParts[2],
-                PhoneNumber: PhoneModified,
-                StreetAddress: StreetAddress[0],
-                City: City,
-                State: State,   
-                StateCode: StateCode,
-                ZipCode: ZipCode,
-                EmailAddress: EmailAddress
+                Person: {
+                  FirstName: nameParts[0], 
+                  LastName: nameParts[2],                  
+                },
+                Location: {
+                  StreetAddress: StreetAddress[0],
+                  City: City,
+                  State: State,   
+                  StateCode: StateCode,
+                  ZipCode: ZipCode,
+                },
+                Contact: {
+                  PhoneNumber: PhoneModified,
+                  EmailAddress: EmailAddress,                  
+                },
+                UserAgent: {
+                  Chrome: chromeAgent.toString(),
+                  Firefox: firefoxAgent.toString(),
+                },
+                Other: {
+                  Username: emailUsername
+                }
             }
         );
     })
